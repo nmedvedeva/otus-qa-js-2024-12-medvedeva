32528f3604594e0e6469172172466e206777715e
Rysaev Damir <freepad@ya.ru>
2024-10-16 16:10:33 +0300

    add arrow functions

--- src/main.js
+++ src/main.js
@@ -1,1 +1,16 @@
+<<<<<<< HEAD
 console.log('Hello!')
+=======
+// Функция
+function greet(name) {
+  return `Hello, ${name}!`
+}
+
+// Стрелочная функция
+const farewell = name => {
+  return `Goodbye, ${name}!`
+}
+
+console.log(greet('World'))
+console.log(farewell('World'))
+>>>>>>> 2b3b4d6 (add arrow functions)
