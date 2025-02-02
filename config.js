50b947a132a3b3a6c01c5d41781b269e8b4591aa
Nadezhda <griinadiine@yandex.ru>
2025-02-02 13:10:10 +0700

    add file api.spec.js for homework 4 (lesson 6)

---
+++ config.js
@@ -0,0 +1,3 @@
+export const login_correct = 'test111'
+export const password_correct = 'Testuserpassword_123!'
+export const password_incorrect = 'pass'
---
+++ specs/api.spec.js
@@ -0,0 +1,76 @@
+import { login_correct, password_correct, password_incorrect } from '../config'
+
+const base_url = 'https://bookstore.demoqa.com'
+
+describe('Auth', () => {
+  it('Test for success user create', async () => {
+    const response = await fetch(base_url + '/Account/v1/User', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        userName: login_correct,
+        password: password_correct
+      })
+    })
+    const data = await response.json()
+    expect(response.status).toEqual(201)
+    expect(data.username).toBe(login_correct)
+  })
+  it('Test busy login', async () => {
+    const response = await fetch(base_url + '/Account/v1/User', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        userName: login_correct,
+        password: password_correct
+      })
+    })
+    const data = await response.json()
+    expect(response.status).toEqual(406)
+    expect(data.message).toBe('User exists!')
+  })
+  it('Test incorrect password', async () => {
+    const response = await fetch(base_url + '/Account/v1/User', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        userName: login_correct,
+        password: password_incorrect
+      })
+    })
+    const data = await response.json()
+    expect(response.status).toEqual(400)
+    expect(data.message).toBeTruthy()
+  })
+})
+
+describe('Test for generate token', () => {
+  it('Tests for success generate token', async () => {
+    const response = await fetch(base_url + '/Account/v1/GenerateToken', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        userName: login_correct,
+        password: password_correct
+      })
+    })
+    const data = await response.json()
+    expect(response.status).toEqual(200)
+    expect(data.status).toBe('Success')
+    expect(data.token).toBeTruthy()
+  })
+  it('Test for success generate token', async () => {
+    const response = await fetch(base_url + '/Account/v1/GenerateToken', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        userName: login_correct,
+        password: password_incorrect
+      })
+    })
+    const data = await response.json()
+    expect(response.status).toEqual(200)
+    expect(data.status).toBe('Failed')
+    expect(data.token).toBeNull()
+  })
+})
