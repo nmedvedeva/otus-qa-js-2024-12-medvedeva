07dec153e4c80b002ba43f76a2ddb49e25974140
Vladimir <52711514+owlzja@users.noreply.github.com>
2025-03-25 17:28:42 +0700

    Update api.spec.ts

--- specs/api.spec.ts
+++ specs/api.spec.ts
@@ -76,7 +76,7 @@ describe('Generate token tests', () => {
     expect(response.data.token).toBeTruthy()
   })
   test('for unsuccessful generate token', async () => {
-    console.log(newUserName)
+    console.log(config.password_incorrect)
     const response = await UserService.generate({
       userName: newUserName,
       password: config.password_incorrect
