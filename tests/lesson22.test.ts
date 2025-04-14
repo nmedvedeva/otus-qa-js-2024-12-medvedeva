c93dda16db9394a6ba041791b56d5dcd1e84f30c
Nadezhda Medvedeva <griinadiine@yandex.ru>
2025-04-14 11:51:00 +0700

    add pageobject and change tests pw

--- tests/lesson22.test.ts
+++ tests/lesson22.test.ts
@@ -1,41 +1,47 @@
-import { expect, test } from '@playwright/test'
+import { test, expect } from '@playwright/test'
+import MainPage from './pages/MainPage'
 
 test('Переход в раздел с проверкой тайтла', async ({ page }) => {
-  await page.goto('https://rus-buket.ru/')
-  await page.getByRole('link', { name: 'Букеты из клубники' }).click()
+  const mainPage = new MainPage(page)
+  await mainPage.gotoHomePage()
+  await mainPage.clickOnBouquetsLink()
   await expect(page).toHaveTitle(/Клубника в шоколаде/)
 })
 
 test('Установка цены в фильтре до 5000', async ({ page }) => {
-  await page.goto('https://rus-buket.ru/')
-  await page.locator('#js_catalog-tags').getByRole('link', { name: 'Букеты', exact: true }).click()
-  const textPriceTo = page.locator('#js_rb-filter__price-to')
-  await textPriceTo.waitFor({ state: 'visible' })
-  await textPriceTo.click()
-  await textPriceTo.fill('')
-  await textPriceTo.pressSequentially('5000')
-  await textPriceTo.press('Enter')
+  const mainPage = new MainPage(page)
+  await mainPage.gotoHomePage()
+  await mainPage.clickOnFilterBouquetsLink()
+  await mainPage.setPriceFilter('5000')
   await expect(
     page.locator('#js_rb-filter-box__tags .rb-filter-box__tags-item').filter({ hasText: 'до 5,000 ₽' })
   ).toHaveText('до 5,000 ₽')
 })
 
 test('Установка цены в фильтре от 2000 до 5000', async ({ page }) => {
-  await page.goto('https://rus-buket.ru/')
-  await page.locator('#js_catalog-tags').getByRole('link', { name: 'Букеты', exact: true }).click()
-  const textPriceFrom = page.locator('#js_rb-filter__price-from')
-  await textPriceFrom.waitFor({ state: 'visible' })
-  await textPriceFrom.click()
-  await textPriceFrom.fill('')
-  await textPriceFrom.pressSequentially('2000')
-  await textPriceFrom.press('Enter')
-  const textPriceTo = page.locator('#js_rb-filter__price-to')
-  await textPriceTo.waitFor({ state: 'visible' })
-  await textPriceTo.click()
-  await textPriceTo.fill('')
-  await textPriceTo.pressSequentially('5000')
-  await textPriceTo.press('Enter')
+  const mainPage = new MainPage(page)
+  await mainPage.gotoHomePage()
+  await mainPage.clickOnFilterBouquetsLink()
+  await mainPage.setPriceRange('2000', '5000')
   await expect(
     page.locator('#js_rb-filter-box__tags .rb-filter-box__tags-item').filter({ hasText: 'от 2,000 ₽ до 5,000 ₽' })
   ).toHaveText('от 2,000 ₽ до 5,000 ₽')
+})
+
+test('Выбор в фильтре "розы"', async ({ page }) => {
+  const mainPage = new MainPage(page)
+  await mainPage.gotoHomePage()
+  await mainPage.clickOnFilterBouquetsLink()
+  await mainPage.clickOnFlowersFilter()
+  await expect(
+    page.locator('#js_rb-filter-box__tags .rb-filter-box__tags-item').filter({ hasText: 'С розами' })
+  ).toHaveText('С розами')
+})
+
+test('Сортировка товаров по цене по возрастанию', async ({ page }) => {
+  const mainPage = new MainPage(page)
+  await mainPage.gotoHomePage()
+  await mainPage.clickOnFilterBouquetsLink()
+  await mainPage.clickOnSortByPrice()
+  await expect(page.locator('#js_sort-box__btn')).toHaveText('Цена по возрастанию')
 })
